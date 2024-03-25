c1fbe99b4a67d833832adfd7f8b04016b1d59441
eyern <eyern254@gmail.com>
2024-03-26 01:46:56 +0300

    file options

--- script.js
+++ script.js
@@ -12,3 +12,11 @@ if (close) {
     nav.classList.remove("active");
   });
 }
+var close_button = document.querySelector(".close-button");
+var social_buttons = document.querySelectorAll(".social");
+
+close_button.addEventListener('click',()=>{
+   social_buttons.forEach(function(buttons){
+       buttons.classList.toggle('hide');
+   });
+});
