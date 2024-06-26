d44cee747869ede3baf0b15e27ed753f9cabc457
eyern <eyern254@gmail.com>
2024-06-26 17:34:19 +0300

    js file

--- script.js
+++ script.js
@@ -20,3 +20,20 @@ close_button.addEventListener('click',()=>{
        buttons.classList.toggle('hide');
    });
 });
+
+function search() {
+  let filter = document.getElementById('find').value.toUpperCase();
+  let item = document.querySelectorAll('.pro');
+  let l = document.getElementsByTagName('h5');
+  for(var i = 0;i<=l.length;i++){
+  let a=item[i].getElementsByTagName('h5')[0];
+  let value=a.innerHTML || a.innerText || a.textContent;
+  if(value.toUpperCase().indexOf(filter) > -1) {
+  item[i].style.display="";
+  }
+  else
+  {
+  item[i].style.display="none";
+  }
+  }
+  }
